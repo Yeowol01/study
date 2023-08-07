00d7fc3c57a7a74143dc894fa3b8c9325f3fe293
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-08-07 19:46:53 +0900

    (2023.08.07) 2차원 배열

---
+++ CLecture/CLecture/(2023.08.04) 이중 포인터.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <stdio.h>
+
+
+int main()
+{
+#pragma region 이중 포인터
+	/*/
+	int data = 100;
+
+	int* ptr1 = &data;
+
+	int** ptr2 = &ptr1;
+
+	**ptr2 = 999;
+
+	printf("data의 값 : %d\n", data);
+	printf("**ptr2의 값 : %d\n", **ptr2);
+	/*/
+
+	/*/
+	int a = 10;
+	int b = 20;
+
+	int* aPtr1 = &a;
+	int** aPtr2 = &aPtr1;
+
+	int* bPtr1 = &b;
+	int** bPtr2 = &bPtr1;
+
+	int* temp= *bPtr2;
+	*bPtr2 = *aPtr2;
+	*aPtr2 = temp;
+
+	printf("**aPtr2가 가리키는 값 : %d\n", **aPtr2);
+	printf("**bPtr2가 가리키는 값 : %d\n", **bPtr2);
+	/*/
+
+#pragma endregion
+
+#pragma region 약수
+
+	/*/
+	int num;
+	printf("입력할 숫자");
+	scanf_s("%d", &num);
+
+	printf("%d의 약수\n", num);
+	for (int i = 1; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			printf("%d\n", i);
+		}
+	}
+	/*/
+
+	// 강사님 방법
+	/*/
+	int value = 0;
+
+	scanf_s("%d", &value);
+
+	for (int i = 1; i <= value; i++)
+	{
+		if (value % i == 0)
+		{
+			printf("%d ", i);
+		}
+	}
+	/*/
+#pragma endregion
+
+
+
+	return 0;
+}
--- CLecture/CLecture/Source.cpp
+++ CLecture/CLecture/Source.cpp
@@ -3,68 +3,104 @@
 
 int main()
 {
-#pragma region 이중 포인터
-	/*/
-	int data = 100;
-
-	int* ptr1 = &data;
-	
-	int** ptr2 = &ptr1;
-
-	**ptr2 = 999;
-
-	printf("data의 값 : %d\n", data);
-	printf("**ptr2의 값 : %d\n", **ptr2);
-	/*/
-
+#pragma region 포인터 배열
 	/*/
 	int a = 10;
 	int b = 20;
+	int c = 30;
 
-	int* aPtr1 = &a;
-	int** aPtr2 = &aPtr1;
+	int* arrayPtr[3] = { &a, &b, &c };
 
-	int* bPtr1 = &b;
-	int** bPtr2 = &bPtr1;
+	printf("arrayPtr[1]의 값 : %p\n", arrayPtr[1]);
+	printf("b의 주소 : %p\n", &b);
 
-	int* temp= *bPtr2;
-	*bPtr2 = *aPtr2;
-	*aPtr2 = temp;
+	*arrayPtr[1] = 200;
 
-	printf("**aPtr2가 가리키는 값 : %d\n", **aPtr2);
-	printf("**bPtr2가 가리키는 값 : %d\n", **bPtr2);
+	printf("arrayPtr[1]이 가리키는 값 : %d\n", *arrayPtr[1]);
 	/*/
-
 #pragma endregion
 
-#pragma region 약수
-	
+#pragma region 짝수와 홀수
 	/*/
 	int num;
+
 	printf("입력할 숫자");
 	scanf_s("%d", &num);
 
-	printf("%d의 약수\n", num);
-	for (int i = 1; i <= num; i++)
+	if (num % 2 == 0)
 	{
-		if (num % i == 0)
-		{
-			printf("%d\n", i);
-		}
+		printf("입력한 정수 %d은 짝수입니다.\n", num);
+	}
+	else
+	{
+		printf("입력한 정수 %d은 홀수입니다.\n", num);
 	}
 	/*/
+#pragma endregion
 
-	// 강사님 방법
+#pragma region 2차원 배열
 	/*/
-	int value = 0;
-	
-	scanf_s("%d", &value);
+	// 배열의 요소로 또 다른 배열을 가지는 배열입니다.
 
-	for (int i = 1; i <= value; i++)
+	// 2차원 배열 또한 연속적인 메모리 공간의 형태로 저장되며, 2차원 배열의 크기를 생략할 때 배열의 열의 크기만 생략할 수 있습니다.
+	int array2D[ ][3] =
 	{
-		if (value % i == 0)
+		{0,1,2},
+		{3,4,5},
+		{6,7,8},
+	};
+
+	// 2차원 배열은 행과 열로 구분되며 앞에 있는 배열은 열을 의미하고, 뒤에 있는 배열은 행을 의미합니다.
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
 		{
-			printf("%d ", i);
+			printf("array2D[i][j]의 값 : %d\n", array2D[i][j]);
+		}
+	}
+	
+	int(*ptr)[3] = array2D;
+
+	printf("ptr의 값 : %p\n", ptr);
+	printf("array2D의 시작 주소 : %p\n", array2D);
+
+	printf("ptr + 1의 시작 주소 : %p\n", ptr + 1);
+	printf("array2D + 1의 시작 주소 : %p\n", array2D + 1);
+	/*/
+#pragma endregion
+
+#pragma region 소인수 분해
+	/*/
+	// while문을 사용한 소인수 분해
+	int i, j = 2;
+
+	scanf_s("%d", &i);
+	
+	while (i > 1)
+	{
+		if (i % j != 0)
+			j++;
+		if (i % j == 0)
+		{
+			printf("%d\n", j);
+			i /= j;
+		}
+	}
+	/*/	
+	
+	// 강사님 방법 (for문 사용)
+	/*/
+	int N = 0;
+
+	scanf_s("%d", &N);
+
+	for (int i = 2; i <= N; i++)
+	{
+		if (N % i == 0)
+		{
+			N /= i;
+			printf("%d\n", i);
+			i--;
 		}
 	}
 	/*/
